605a80eb15a9f74b184bfdf83a46bef5393f1fbc
pasanjg <44201430+pasanjg@users.noreply.github.com>
2020-10-08 08:32:34 +0530

    Switch Modes

--- assets/script.js
+++ assets/script.js
@@ -4,15 +4,21 @@ function refresh() {
 }
 
 function erase() {
-
   var editorText = document.getElementById('editor').value;
-
   if ((editorText !== null) || (editorText !== ' ') || (editorText === undefined)) {
     if (confirm('Clear everything in the editor?')) {
       document.getElementById('editor').value = "";
       refresh();
     }
   }
+}
+
+function switchMode() {
+  document.body.classList.toggle('light');
+  if (document.getElementById("switch").classList.contains('fa-moon'))
+    document.getElementById("switch").classList.toggle('fa-sun');
+  else
+    document.getElementById("switch").classList.toggle('fa-moon');
 }
 
 window.onbeforeunload = function () {
